13a2ff2f25d695f753eb0623931fbcc9c3a6ea68
lomholdt <jonaslomholdt@gmail.com>
2020-03-15 12:48:33 +0100

    Update selectors

--- lambda/get-corona-status/get-corona-status.js
+++ lambda/get-corona-status/get-corona-status.js
@@ -15,10 +15,10 @@ exports.handler = async function(event, context) {
     const data = await response.text()
     const $ = cheerio.load(data);
 
-    const tested = $('body > div.main__content > main > article > div.o-content-block.u-grid.u-grid--space-between.u-grid--no-gutter.u-ie > div > div:nth-child(4) > div > table > tbody > tr:nth-child(2) > td:nth-child(2)');
-    const infected = $('body > div.main__content > main > article > div.o-content-block.u-grid.u-grid--space-between.u-grid--no-gutter.u-ie > div > div:nth-child(4) > div > table > tbody > tr:nth-child(2) > td:nth-child(3)');
-    const deaths = $('body > div.main__content > main > article > div.o-content-block.u-grid.u-grid--space-between.u-grid--no-gutter.u-ie > div > div:nth-child(4) > div > table > tbody > tr:nth-child(2) > td:nth-child(4)');
-    const updatedAt = $('body > div.main__content > main > article > div.o-content-block.u-grid.u-grid--space-between.u-grid--no-gutter.u-ie > div > div:nth-child(4) > p:nth-child(4) > em > em');
+    const tested = $('body > div.main__content > main > article > div.o-content-block.u-grid.u-grid--space-between.u-grid--no-gutter.u-ie > div > div:nth-child(5) > div > table > tbody > tr:nth-child(2) > td:nth-child(2)');
+    const infected = $('body > div.main__content > main > article > div.o-content-block.u-grid.u-grid--space-between.u-grid--no-gutter.u-ie > div > div:nth-child(5) > div > table > tbody > tr:nth-child(2) > td:nth-child(3)');
+    const deaths = $('body > div.main__content > main > article > div.o-content-block.u-grid.u-grid--space-between.u-grid--no-gutter.u-ie > div > div:nth-child(5) > div > table > tbody > tr:nth-child(2) > td:nth-child(4)');
+    const updatedAt = $('body > div.main__content > main > article > div.o-content-block.u-grid.u-grid--space-between.u-grid--no-gutter.u-ie > div > div:nth-child(5) > p:nth-child(3) > em > em');
 
     return {
       statusCode: 200,
